b8583931ecca151ffce7371c63681411163bb9ef
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-07-21 11:06:20 +0200

    fix android app

---
+++ room-app/RoomApp/app/src/main/java/com/example/roomapp/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.roomapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@SuppressLint("MissingPermission")
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonOn;
+    private Button buttonOff;
+    private SeekBar seekBar;
+    private BluetoothClientConnectionThread connectionThread;
+    private OutputStream bluetoothOutputStream;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        initUI();
+    }
+
+    private void initUI(){
+        buttonOn = findViewById(R.id.buttonOn);
+        buttonOff = findViewById(R.id.buttonOff);
+        buttonOn.setEnabled(true);
+        buttonOff.setEnabled(true);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.d("SeekBar", "" + progress);
+                sendMessage(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        buttonOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("BUTTON", "Invio richiesta");
+                sendMessage("ON");
+            }
+        });
+
+        buttonOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("BUTTON", "Invio richiesta");
+                sendMessage("OFF");
+            }
+        });
+
+    }
+
+    private void sendMessage(String buttonState) {
+        new Thread(() -> {
+            try {
+                String message =  "{\"State\":\""+buttonState+"\"}\n";
+                Log.d("Type message", " "+message);
+                bluetoothOutputStream.write(message.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = getIntent();
+        BluetoothDevice bluetoothDevice = intent.getParcelableExtra(ScanActivity.X_BLUETOOTH_DEVICE_EXTRA);
+        BluetoothAdapter btAdapter = getSystemService(BluetoothManager.class).getAdapter();
+        Log.i (C.TAG, "Connecting to " + bluetoothDevice.getName());
+        connectionThread = new BluetoothClientConnectionThread(bluetoothDevice, btAdapter, socket -> manageConnectedSocket(socket));
+        connectionThread.start();
+    }
+
+    private void manageConnectedSocket(BluetoothSocket socket) {
+        try {
+            bluetoothOutputStream = socket.getOutputStream();
+            //emulatedBluetoothOutputStream = socket.getOutputStream();
+            Log.i(C.TAG, "Connection successful!");
+        } catch (IOException e) {
+            Log.e(C.TAG, "Error occurred when creating output stream", e);
+        }
+        runOnUiThread(() -> {
+            buttonOn.setEnabled(true);
+            buttonOff.setEnabled(true);
+            seekBar.setEnabled(true);
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        connectionThread.cancel();
+    }
+
+}
